aba6c4288fa8a05b13dd7d222b124e9bf02ea2da
Andi <andi.winata@hotmail.com>
2017-01-29 18:49:20 +1100

    Adding next button, before adding redux to client

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,31 +1,11 @@
 import React from 'react';
+import Winner from './Winner';
+import TallyResult from './TallyResult';
 
 export default class Results extends React.PureComponent {
-    constructor(props) {
-        super(props);
-
-        this.getPair = this.getPair.bind(this);
-    }
-
-    getPair() {
-        return this.props.pair;
-    }
-
-    getVotes(entry) {
-        if (this.props.tally && this.props.tally.has(entry)) {
-            return this.props.tally.get(entry);
-        }
-        return 0;
-    }
-
     render() {
-        return <div>
-            {this.getPair().map(entry => 
-                <div key={entry} className="entry">
-                    <h1>{entry}</h1>
-                    <div className="voteCount">{this.getVotes(entry)}</div>
-                </div>
-            )}
-        </div>;
+        return this.props.winner ? 
+            <Winner winner={this.props.winner}></Winner> :
+            <TallyResult {...this.props} />;
     }
 }
---
+++ src/components/TallyResult.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default class TallyResult extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        // not sure if necessary, since it is not passed to any children
+        this.getPair = this.getPair.bind(this);
+        this.getVotes = this.getVotes.bind(this);
+    }
+    
+    getPair() {
+        return this.props.pair;
+    }
+
+    getVotes(entry) {
+        if (this.props.tally && this.props.tally.has(entry)) {
+            return this.props.tally.get(entry);
+        }
+        return 0;
+    }
+
+    render() {
+        return <div className="tally-result">
+            <div className="tally">
+                {this.getPair().map(entry =>
+                    <div key={entry} className="entry">
+                        <h1>{entry}</h1>
+                        <div className="voteCount">{this.getVotes(entry)}</div>
+                    </div>
+                )}
+            </div>
+            <div className="management">
+                <button className="next" onClick={this.props.next}>
+                    Next
+                </button>
+            </div>
+        </div>
+    };
+}
