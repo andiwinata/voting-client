2b20202d208a3417f2dfd7007cdaa890f7aa66db
Andi <andi.winata@hotmail.com>
2017-02-08 22:12:12 +1100

    Removing next from reducer, Updating client side RESET_VOTING reducer

--- src/reducer.js
+++ src/reducer.js
@@ -30,17 +30,14 @@ function resetVote(state) {
 export default function (state = Map(), action) {
     switch (action.type) {
         case 'SET_STATE':
-            console.log('RECEIVE SET SET_STATE', action.state);
             return resetVote(setState(state, action.state));
         case 'VOTE':
             return vote(state, action.entry);
         case 'SET_CLIENT_ID':
             return state.set('clientId', action.clientId);
-        case 'NEXT':
-            return state.remove('hasVoted');
         case 'RESET_VOTING':
             return state.remove('winner')
-                .remove('hasVoted');
+                .remove('myVote');
     }
     return state;
 }
